e491ad002d1ecd53e65f553c2de94ff9f81bb576
mac <wubezeleke@gmail.com>
2024-03-15 14:41:51 +0300

    initial commit to configure django with tailwind CSS

---
+++ food_truck_finder/food_trucks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FoodTrucksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'food_trucks'
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./food_truck_finder/food_trucks/templates/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
