81c964507b56021476c6078b82edb7a4d67486bf
mac <wubezeleke@gmail.com>
2024-03-16 00:35:14 +0300

    Fix: Add getDatabase to Setting

--- food_truck_finder/food_truck_finder/settings.py
+++ food_truck_finder/food_truck_finder/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from utils.database import get_database
+
+get_database()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
