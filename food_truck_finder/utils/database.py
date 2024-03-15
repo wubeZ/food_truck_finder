09837dda2a162378970c4a64cd5c47173521267d
mac <wubezeleke@gmail.com>
2024-03-16 00:29:45 +0300

    Feat: Add DB connnection setup

---
+++ food_truck_finder/utils/database.py
@@ -0,0 +1,27 @@
+import os
+import mongoengine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Global database connection instance
+db_connection = None
+
+def connect_to_mongodb_atlas(mongo_url):
+    global db_connection
+    if db_connection is None:
+        try:
+            # Connect to MongoDB Atlas
+            db_connection = mongoengine.connect(host=mongo_url,alias='default')
+            print("Connected to MongoDB Atlas successfully.")
+        except Exception as e:
+            print(f"Error connecting to MongoDB Atlas: {e}")
+
+    return db_connection
+
+def get_database():
+    
+    mongo_url = os.getenv("MONGO_URL", "localhost:/2710")
+    
+    # Connect to MongoDB Atlas and return the database connection instance
+    return connect_to_mongodb_atlas(mongo_url)
