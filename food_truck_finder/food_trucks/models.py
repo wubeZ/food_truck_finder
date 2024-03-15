3a6d141c8ff723d0729176626524393fc415649e
mac <wubezeleke@gmail.com>
2024-03-16 00:28:57 +0300

    Feat: Add Food Truck Model

---
+++ food_truck_finder/food_trucks/models.py
@@ -0,0 +1,12 @@
+from mongoengine import Document, StringField, ListField, DictField, PointField
+
+class FoodTruck(Document):
+    name = StringField(required=True)
+    facility_type = StringField(required=True)
+    location_description = StringField(required=True)
+    address = StringField(required=True)
+    permit = StringField(required=True)
+    status = StringField(required=True)
+    food_items = ListField(StringField())
+    location = PointField(auto_index=False, required=True)
+    open_hours = DictField()
